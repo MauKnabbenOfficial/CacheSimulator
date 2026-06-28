1e23d543386d5068e4cd99c5f180d67b4140a6e4
Mauricio Knabben <knabben.mau@gmail.com>
2026-06-28 19:40:18 -0300

    .bat

--- build_and_run.bat
+++ build_and_run.bat
@@ -4,3 +4,6 @@ echo ========== dotnet build ========== > build_output.txt 2>&1
 dotnet build >> build_output.txt 2>&1
 echo. >> build_output.txt
 echo ========== DONE ========== >> build_output.txt
+echo.
+echo ========== dotnet run ==========
+dotnet run
